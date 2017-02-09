611f994d1b012c896fd6d24a2111382463b8aa08
Pierre Desmet <spellgo@hotmail.fr>
2017-02-09 08:21:46 +0100

    Todel
    Fichier php à supprimer par la suite

---
+++ tp02.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>
+	CV de Pierre Desmet	
+	</title>
+	<!-- La feuille de styles "base.css" doit être appelée en premier. -->
+	<link rel="stylesheet" type="text/css" href="styles/base.css" media="all" />
+	<link rel="stylesheet" type="text/css" href="tp02_exo2.css" media="screen" />
+</head>
+
+<body>
+
+<div id="global">
+
+<?php include("header.php");?>
+	
+<?php include("menu.php");?>
+	
+	<div id="contenu">
+		<strong>TP02</strong>
+		<ul>
+		<li><a href = "tp02_exo1.html">Exercice 1</a></li>
+		<li><a href = "tp02_exo2.php">Exercice 2</a></li>
+		<li><a href = "tp02_exo3.php">Exercice 3</a></li>
+		<li><a href = "tp02_exo4.php">Exercice 4</a></li>
+		</ul>
+
+
+	</div><!-- #contenu -->
+
+	<div id="pied">
+		<p>Pierre Desmet &copy 2014  <a href = "https://fr-fr.facebook.com/Spiderpeter">Facebook</a>
+		<a href = "https://twitter.com/Lactu_cine">Twitter</a>
+		<a href = "https://www.worldcubeassociation.org/results/p.php?i=2012DESM02">WCA</a>
+			</p>
+			<!--<p id="copyright">
+			Mise en page ©; 2008
+			<a href="http://www.elephorm.com">Elephorm</a> et
+			<a href="http://www.alsacreations.com">Alsacréations</a>
+		</p>--!>
+		
+	</div><!-- #pied -->
+
+</div><!-- #global -->
+
+</body>
+</html>
